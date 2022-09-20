64e3bd7db92e619eafb966a93dce71b8fb1cff61
UKuskus <65870521+UKuskus@users.noreply.github.com>
2022-09-21 00:14:44 +0300

    Components Header,Nav,Content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
-import Header from './Header';
-import Navigation from './Navigation';
-import Content from './Content';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Content from './components/Content';
 
 
 const App = () => {
---
+++ src/components/Content.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Content = () =>{
+    return(
+      <div className='Content'>
+        <div>
+          <img alt="background-photo" src='https://images.ctfassets.net/hrltx12pl8hq/5596z2BCR9KmT1KeRBrOQa/4070fd4e2f1a13f71c2c46afeb18e41c/shutterstock_451077043-hero1.jpg'/>
+        </div>
+
+        <div>Profile photo + description</div>
+        <div>
+          <div>
+            My posts
+            <div>
+              New post
+            </div>
+              <div>
+                <div>
+                  Post 1
+                </div>
+                <div>
+                  Post 2
+                </div>
+              </div>
+          </div>
+        </div>
+      
+    </div>
+    );
+}
+
+export default Content;
