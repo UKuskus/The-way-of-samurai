43d3f6b808794d1c44e37803e137e0b682b15b48
UKuskus <65870521+UKuskus@users.noreply.github.com>
2022-09-20 19:41:16 +0300

    Import/export

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Series/>
     </div>
+
+  );
+}
+const Header = () => {
+  return (
+    <div className="Header">
+        <a href='#c'>News-</a>
+        <a href='#c'>Feed-</a>
+        <a href='#c'>Updates-</a>
+        <a href='#c'>Messages</a>
+    </div>);
+}
+const Series = () =>{
+  return(
+    <div className="Series">
+    <ul>
+      <li>Rick and Morty</li>
+      <li>Simpsons</li>
+      <li>Griffins</li>
+    </ul> 
+  </div>
   );
 }
 
