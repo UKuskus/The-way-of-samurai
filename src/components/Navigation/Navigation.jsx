3f0ffe5de98c1a0cf656e1b66c7be0d56121ffed
UKuskus <65870521+UKuskus@users.noreply.github.com>
2022-11-09 17:59:24 +0200

    NavLink + active click

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s from './Navigation.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navigation = () => {
   return (
     <nav className={s.Navigation}>
             <div className={s.NavItem}>
-        <a href="/profile">Profile</a>
+        <NavLink className={({ isActive }) => (isActive ? s.active : '')} to="/profile">Profile</NavLink>
       </div>
       <div className={s.NavItem}>
-        <a href="/dialogs">Messages</a>
+        <NavLink className={({ isActive }) => (isActive ? s.active : '')} to="/dialogs">Messages</NavLink>
       </div>
       <div className={s.NavItem}>
-        <a href="/news">News</a>
+        <NavLink className={({ isActive }) => (isActive ? s.active : '')} to="/news">News</NavLink>
       </div>
       <div className={s.NavItem}>
-        <a href="/music">Music</a>
+        <NavLink className={({ isActive }) => (isActive ? s.active : '')} to="/music">Music</NavLink>
       </div>
       <div className={s.NavItem}>
-        <a href="/settings">Settings</a>
+        <NavLink className={({ isActive }) => (isActive ? s.active : '')}  to="/settings">Settings</NavLink>
       </div>
     </nav>
   );
